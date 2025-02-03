9760ecb12d56ed7f9b8ea115b74b1631bb99edff
jeoungsung12 <jeoungsung12@naver.com>
2025-02-03 19:30:36 +0900

    Add: PortfolioVC 에러 처리

--- FinancialApp/Feature/Portfolio/PortfolioViewModel.swift
+++ FinancialApp/Feature/Portfolio/PortfolioViewModel.swift
@@ -16,57 +16,72 @@ final class PortfolioViewModel {
     }
     
     struct Output {
-        //TODO: - 에러 처리
-        let portfolioOutput : Observable<[PortfolioModel]>
+        let portfolioOutput : Observable<Result<[PortfolioModel],NetworkError.CustomError>>
     }
     
     func transform(input: Input) -> Output {
-        //TODO: - DispatchGroup
         let chartOutput = input.portfolioInput
-            .flatMapLatest { [weak self] db -> Observable<[PortfolioModel]> in
+            .flatMapLatest { [weak self] db -> Observable<Result<[PortfolioModel],NetworkError.CustomError>> in
+                guard self != nil else { return Observable.empty() }
                 let cryptoArray: [String] = (db.isEmpty) ? [] : db.map ({ $0.name })
                 let fillteredData = cryptoData.filter { cryptoArray.contains($0.market) }
-                guard self != nil else { return Observable.empty() }
-                let ticksObservable = OrderBookService().getTotal(totalData: Array(fillteredData))
                 
-                return ticksObservable.flatMap { ticksData -> Observable<[PortfolioModel]> in
-                    guard let self = self else { return Observable.empty() }
-                    let priceDict: [String: Double] = ticksData
-                        .map { $0 }
-                        .reduce(into: [String: Double]()) { dict, tick in
-                            guard let tickData = tick.first else { return }
-                            dict[tickData.tradesData.market] = tickData.tradesData.trade_price
+                
+                let ticksObservable = OrderBookService().getTotal(totalData: Array(fillteredData))
+                    .map { (response: Result<[[AddTradesModel]],NetworkError.CustomError>) -> Result<[[AddTradesModel]],NetworkError.CustomError> in
+                        switch response {
+                        case let .success(data):
+                            return .success(data)
+                        case let .failure(error):
+                            return .failure(error)
                         }
-                    
-                    let priceList = db.compactMap { priceDict[$0.name] }
-                    let rateList = self.calculateRate(priceList, db)
-                    
-                    let portfolioModels = zip(db, priceList).compactMap { (dbItem, openPrice) -> PortfolioModel? in
-                        guard let quantity = Double(dbItem.quantity),
-                              let price = Double(dbItem.price),
-                              let name = cryptoData.first(where: { $0.market == dbItem.name }) else { return nil }
-                        
-                        return PortfolioModel(
-                            name: name.korean_name,
-                            quantity: quantity,
-                            purchasePrice: price,
-                            currentPrice: openPrice,
-                            rate: rateList
-                        )
                     }
-
-                    return Observable.just(portfolioModels)
-
+                
+                
+                return ticksObservable.flatMap { (ticksData: Result<[[AddTradesModel]], NetworkError.CustomError>) -> Observable<Result<[PortfolioModel], NetworkError.CustomError>> in
+                    guard let self = self else { return .just(.failure(.notFount)) }
+                    
+                    switch ticksData {
+                    case let .success(data):
+                        let priceDict: [String: Double] = data
+                            .flatMap { $0 }
+                            .reduce(into: [String: Double]()) { dict, tick in
+                                dict[tick.tradesData.market] = tick.tradesData.trade_price
+                            }
+                        
+                        let priceList = db.compactMap { priceDict[$0.name] }
+                        let rateList = self.calculateRate(priceList, db)
+                        
+                        let portfolioModels = zip(db, priceList).compactMap { (dbItem, openPrice) -> PortfolioModel? in
+                            guard let quantity = Double(dbItem.quantity),
+                                  let price = Double(dbItem.price),
+                                  let name = cryptoData.first(where: { $0.market == dbItem.name }) else { return nil }
+                            
+                            return PortfolioModel(
+                                name: name.korean_name,
+                                quantity: quantity,
+                                purchasePrice: price,
+                                currentPrice: openPrice,
+                                rate: rateList
+                            )
+                        }
+                        
+                        return .just(.success(portfolioModels))
+                        
+                    case .failure(let error):
+                        return .just(.failure(error))
+                    }
                 }
+                
             }
-        
         return Output(portfolioOutput: chartOutput)
     }
     
+    //TODO:- 수정
     private func calculateRate(_ openingPrices: [Double],_ db: [HeartItem]) -> Double {
         let heartList = db
         guard !heartList.isEmpty, !openingPrices.isEmpty else { return 0 }
-
+        
         var totalInvested: Double = 0
         var totalCurrentValue: Double = 0
         
